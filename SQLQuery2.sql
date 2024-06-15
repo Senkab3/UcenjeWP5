567420ebe018f0c382113c4c7b56abb2d74cdb86
Senka Banjac <sbanjac88@gmail.com>
2024-06-15 11:42:53 +0200

    fotografski studio
    fotografski studio

---
+++ SQLQuery2.sql
@@ -0,0 +1,21 @@
+--use master;
+--go
+--drop database if exists fotografski_studio;
+--go
+--create database fotografski_studio;
+--go
+--use fotografski_studio;
+
+--create table Djelatnik(
+--sifra int not null primary key identity,
+--ime varchar(2),
+--prezime varchar(2),
+--oib char(11)
+--);
+
+--create table Usluga(
+--sifra int not null primary key identity,
+--naziv varchar(4) not null,
+--cijena decimal (18,2),
+--djelatnik varchar(2)
+--);
